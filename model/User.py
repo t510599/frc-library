0b7775e8bd6bcd4d9830d707dea249f0403ee873
secminhr <secminhrian@gmail.com>
2019-07-20 22:05:59 +0800

    login feature

--- model/User.py
+++ model/User.py
@@ -1,5 +1,9 @@
-class User:
+from flask_login import UserMixin
+class User(UserMixin):
 	def __init__(self, uid, username, encoding):
 		self.uid = uid
 		self.username = username
 		self.encoding = encoding
+
+	def get_id(self):
+		return str(self.uid)
